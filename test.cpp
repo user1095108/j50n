5ae989b05e2af3cfa285914d4019c836fae6a3de
anon <>
2023-04-16 16:25:29 +0200

    some fixes

--- test.cpp
+++ test.cpp
@@ -9,14 +9,14 @@ int main()
   auto const s((std::ostringstream() << std::ifstream("test.json").rdbuf()).str());
   j50n j(s);
 
-  std::cout << j["bool"].get() << std::endl;
-  std::cout << j["foo"].get() << std::endl;
-  std::cout << j["test"].get() << std::endl;
-  std::cout << j["key"].get() << std::endl;
-  std::cout << j["obj"].get() << std::endl;
-  std::cout << j["array"].get() << " " << j["array"].size() << std::endl;
-  std::cout << j.find("array", 0).get() << " " << j["array"][2].get() << std::endl;
-  std::cout << j.find("obj", "true").get() << std::endl;
+  std::cout << j["bool"] << std::endl;
+  std::cout << j["foo"] << std::endl;
+  std::cout << j["test"] << std::endl;
+  std::cout << j["key"] << std::endl;
+  std::cout << j["obj"] << std::endl;
+  std::cout << j["array"] << " " << j["array"].size() << std::endl;
+  std::cout << j.find("array", 0) << " " << j["array"][2] << std::endl;
+  std::cout << j.find("obj", "true") << std::endl;
 
   return 0;
 }
